5570d7313ccc72dcc6391999a15ff2099fbcb174
Mitchel Perekebuna Edah-Ekubo <edahmitchel@gmail.com>
2023-10-13 19:40:02 +0100

    ...

--- dist/src/routes/index.js
+++ dist/src/routes/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
 var shortUrl_controller_1 = require("../controllers/shortUrl.controller");
 var validateResource_1 = __importDefault(require("../middleware/validateResource"));
 var createShortUrl_schema_1 = __importDefault(require("../schemas/createShortUrl.schema"));
@@ -10,130 +11,129 @@ var auth_controller_1 = require("../controllers/auth.controller");
 var registration_schema_1 = __importDefault(require("../schemas/registration.schema"));
 var login_schema_1 = __importDefault(require("../schemas/login.schema"));
 var auth_middleware_1 = require("../middleware/auth.middleware");
-function routes(app) {
-    /**
+var router = express_1.default.Router();
+/**
+* @swagger
+* /check:
+*   get:
+*     summary: Check if the API is working
+*     responses:
+*       200:
+*         description: API is working
+*       500:
+*         description: API is not working
+*/
+router.get("/check", function (req, res) {
+    return res.send("works");
+});
+// Authentication routes
+/**
+* @swagger
+* /auth/signup:
+*   post:
+*     summary: Register a new user
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               username:
+*                 type: string
+*               email:
+*                 type: string
+*                 format: email
+*               password:
+*                 type: string
+*     responses:
+*       201:
+*         description: User registered successfully
+*       500:
+*         description: User registration failed
+*/
+router.post('/auth/signup', (0, validateResource_1.default)(registration_schema_1.default), auth_controller_1.registerUser);
+/**
+* @swagger
+* /auth/login:
+*   post:
+*     summary: Log in as an existing user
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               email:
+*                 type: string
+*                 format: email
+*               password:
+*                 type: string
+*     responses:
+*       200:
+*         description: Login successful
+*       401:
+*         description: User not found or incorrect password
+*       500:
+*         description: Login failed
+*/
+router.post('/auth/login', (0, validateResource_1.default)(login_schema_1.default), auth_controller_1.loginUser);
+//   
+/**
+* @swagger
+* /api/url:
+*   post:
+*     summary: Create a short URL
+*     security:
+*       - BearerAuth: []
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               destination:
+*                 type: string
+*     responses:
+*       201:
+*         description: Short URL created successfully
+*       500:
+*         description: Failed to create short URL
+*/
+router.post("/api/url", auth_middleware_1.authenticateToken, (0, validateResource_1.default)(createShortUrl_schema_1.default), shortUrl_controller_1.createShortUrl);
+/**
  * @swagger
- * /check:
+ * /short/{shortId}:
  *   get:
- *     summary: Check if the API is working
+ *     summary: Redirect to the original URL using a short ID
+ *     parameters:
+ *       - in: path
+ *         name: shortId
+ *         required: true
+ *         description: Short ID of the URL
+ *         schema:
+ *           type: string
  *     responses:
- *       200:
- *         description: API is working
- *       500:
- *         description: API is not working
+ *       302:
+ *         description: Redirects to the original URL
+ *       404:
+ *         description: Short URL not found
  */
-    app.get("/check", function (req, res) {
-        return res.send("works");
-    });
-    // Authentication routes
-    /**
-    * @swagger
-    * /auth/signup:
-    *   post:
-    *     summary: Register a new user
-    *     requestBody:
-    *       required: true
-    *       content:
-    *         application/json:
-    *           schema:
-    *             type: object
-    *             properties:
-    *               username:
-    *                 type: string
-    *               email:
-    *                 type: string
-    *                 format: email
-    *               password:
-    *                 type: string
-    *     responses:
-    *       201:
-    *         description: User registered successfully
-    *       500:
-    *         description: User registration failed
-    */
-    app.post('/auth/signup', (0, validateResource_1.default)(registration_schema_1.default), auth_controller_1.registerUser);
-    /**
-    * @swagger
-    * /auth/login:
-    *   post:
-    *     summary: Log in as an existing user
-    *     requestBody:
-    *       required: true
-    *       content:
-    *         application/json:
-    *           schema:
-    *             type: object
-    *             properties:
-    *               email:
-    *                 type: string
-    *                 format: email
-    *               password:
-    *                 type: string
-    *     responses:
-    *       200:
-    *         description: Login successful
-    *       401:
-    *         description: User not found or incorrect password
-    *       500:
-    *         description: Login failed
-    */
-    app.post('/auth/login', (0, validateResource_1.default)(login_schema_1.default), auth_controller_1.loginUser);
-    //   
-    /**
- * @swagger
- * /api/url:
- *   post:
- *     summary: Create a short URL
- *     security:
- *       - BearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               destination:
- *                 type: string
- *     responses:
- *       201:
- *         description: Short URL created successfully
- *       500:
- *         description: Failed to create short URL
- */
-    app.post("/api/url", auth_middleware_1.authenticateToken, (0, validateResource_1.default)(createShortUrl_schema_1.default), shortUrl_controller_1.createShortUrl);
-    /**
-     * @swagger
-     * /short/{shortId}:
-     *   get:
-     *     summary: Redirect to the original URL using a short ID
-     *     parameters:
-     *       - in: path
-     *         name: shortId
-     *         required: true
-     *         description: Short ID of the URL
-     *         schema:
-     *           type: string
-     *     responses:
-     *       302:
-     *         description: Redirects to the original URL
-     *       404:
-     *         description: Short URL not found
-     */
-    app.get("/short/:shortId", shortUrl_controller_1.handleRedirect);
-    /**
-    * @swagger
-    * /api/analytics:
-    *   get:
-    *     summary: Retrieve analytics data for short URLs
-    *     responses:
-    *       200:
-    *         description: Analytics data retrieved successfully
-    *       500:
-    *         description: Failed to retrieve analytics data
-    *         content:application/json:
-   
-    */
-    app.get("/api/analytics", shortUrl_controller_1.getAnalytics);
-}
-exports.default = routes;
+router.get("/short/:shortId", shortUrl_controller_1.handleRedirect);
+/**
+* @swagger
+* /api/analytics:
+*   get:
+*     summary: Retrieve analytics data for short URLs
+*     responses:
+*       200:
+*         description: Analytics data retrieved successfully
+*       500:
+*         description: Failed to retrieve analytics data
+*         content:application/json:
+ 
+*/
+router.get("/api/analytics", shortUrl_controller_1.getAnalytics);
+exports.default = router;
--- dist/src/server.js
+++ dist/src/server.js
@@ -4,18 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var routes_1 = __importDefault(require("./routes"));
 var db_1 = __importDefault(require("./database/db"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+var index_1 = __importDefault(require("./routes/index"));
 var swagger_1 = __importDefault(require("./swagger")); // Import the Swagger configuration
 dotenv_1.default.config();
 var app = (0, express_1.default)();
-var port = process.env.port;
+var port = process.env.PORT;
+(0, db_1.default)();
 app.use(express_1.default.json());
+app.use("/api", index_1.default);
 app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_1.default));
 app.listen(port, function () {
     console.log("listening on port ".concat(port));
-    (0, db_1.default)();
-    (0, routes_1.default)(app);
 });
